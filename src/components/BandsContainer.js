ef19905d234283a8fa1668b8304fc6af549a9a1b
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-13 18:29:36 +0000

    Automatically backed up by Learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -18,8 +18,8 @@ const mapStateToProps = ({ bands }) => ({ bands })
 
 const mapDispatchToProps = dispatch => ({
   addBand: name => dispatch({ 
-    type: "ADD_BAND", name 
-    
+    addBand: name => dispatch({ type: "ADD_BAND", name }),
+    deleteBand: id => dispatch({type: "DELETE_BAND", id}) 
   })
 })
 

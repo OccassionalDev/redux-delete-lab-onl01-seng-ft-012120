a3b950a327d17c53d2643cc784992f1e5b77afee
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-13 18:38:31 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Band from './Band'
 
 const Bands = props => {
-  const bandsObj = props.bands.map(band => <Band key={band.id} {...band} deleteBand={ props.deleteBand }/>)
+  const bands = props.bands.map(band => <Band key={band.id} {...band} deleteBand={ props.deleteBand }/>)
   
   return (
     <div>
-      { bandsObj }
+      { bands }
     </div>
     )
 }

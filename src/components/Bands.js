2de768bb7ee806c94117003e77059ccd422e3a5e
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-13 18:27:58 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -4,6 +4,9 @@ import Band from './Band'
 const Bands = props => {
   const bands = props.bands.map(band => <Band key={band.id} {...band} deleteBand={props.deleteBand}/>)
   
-  
-
+  return (
+    <div>
+      { bands }
+    </div>
+    )
 }

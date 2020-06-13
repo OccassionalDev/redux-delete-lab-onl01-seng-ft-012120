d6717028714700e1a894ca7881e941e400635cd6
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-13 18:33:13 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -13,7 +13,8 @@ export default function manageBand(state = {
       return { ...state, bands: [...state.bands, band] }
 
     case 'DELETE_BAND':
-      
+            const bands = state.bands.filter(band => band.id !== action.id);
+      return { bands };
 
     default:
       return state;
